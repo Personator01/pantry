16d194a473b708090b7e94212a57e03252418565
Ty Butler <tytbu@tytbutler.com>
2024-11-12 19:50:31 -0600

    init

---
+++ Pantry/Platforms/Windows/App.fs
@@ -0,0 +1,9 @@
+﻿namespace Pantry.WinUI
+
+/// <summary>
+/// Provides application-specific behavior to supplement the default Application class.
+/// </summary>
+type App() =
+    inherit FSharp.Maui.WinUICompat.App()
+
+    override this.CreateMauiApp() = Pantry.MauiProgram.CreateMauiApp()
