a0f2d1e32d57b7791103623539176c90970a43f1
Ty Butler <tytbu@tytbutler.com>
2024-11-15 17:27:20 -0600

    nuked old version

--- Pantry/Platforms/Windows/App.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pantry.WinUI
-
-/// <summary>
-/// Provides application-specific behavior to supplement the default Application class.
-/// </summary>
-type App() =
-    inherit FSharp.Maui.WinUICompat.App()
-
-    override this.CreateMauiApp() = Pantry.MauiProgram.CreateMauiApp()
--- Pantry/Platforms/Windows/Main.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pantry.WinUI
-
-open System
-
-module Program =
-    [<EntryPoint; STAThread>]
-    let main args =
-        do FSharp.Maui.WinUICompat.Program.Main(args, typeof<Pantry.WinUI.App>)
-        0
